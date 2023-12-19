5ea623d0d3763b966608313aab97cad1f75681ef
Mark <mbennett5516@yahoo.com>
2023-12-19 11:49:04 -0600

    initial commit: Spring/Maven project structure and db config

---
+++ src/main/java/com/bennett/TabletopCharacterManager/TabletopCharacterManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bennett.TabletopCharacterManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabletopCharacterManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabletopCharacterManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bennett/TabletopCharacterManager/TabletopCharacterManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bennett.TabletopCharacterManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabletopCharacterManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
